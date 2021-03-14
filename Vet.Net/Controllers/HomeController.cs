7bb9affa6a6ee2fdb14c26e704edfadbe92ec0c9
Mark Adrian Go <11915007@benilde.edu.ph>
2021-03-14 14:26:45 +0800

    Go-Sunday_Push

--- Vet.Net/Controllers/HomeController.cs
+++ Vet.Net/Controllers/HomeController.cs
@@ -27,11 +27,15 @@ namespace Vet.Net.Controllers
         {
             return View();
         }
-
+        public IActionResult ReserveForm()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
