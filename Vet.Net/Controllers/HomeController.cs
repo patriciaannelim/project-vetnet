041d37355b60c6e7e52a51f8d646b6fb9ccd2fd4
Patricia Lim <patriciaaxlim@gmail.com>
2021-04-19 18:20:35 +0800

    Register - Updated // Contact Us Created

--- Vet.Net/Controllers/HomeController.cs
+++ Vet.Net/Controllers/HomeController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Vet.Net.Models;
 
+using System.Net;
+using System.Net.Mail;
+
 namespace Vet.Net.Controllers
 {
     public class HomeController : Controller
@@ -35,6 +38,42 @@ namespace Vet.Net.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(Contact record)
+        {
+            using (MailMessage mail = new MailMessage("entprog.vet.net@gmail.com", record.Email))
+            {
+                mail.Subject = record.Subject;
+
+                string message = "Hello, " + record.SenderName + "!<br/><br/>" +
+                "We have received you inquiry. Here are the details: <br/><br/>" +
+                "Contact Number: <strong>" + record.ContactNo + "</strong><br/>" +
+                "Message: <br/><strong>" + record.Message + "</strong><br/><br/>" +
+                "Please wait for our reply. Thank you!";
+                mail.Body = message;
+                mail.IsBodyHtml = true;
+
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.EnableSsl = true;
+                    NetworkCredential NetworkCred =
+                        new NetworkCredential("entprog.vet.net@gmail.com", "entprogproject");
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = NetworkCred;
+                    smtp.Port = 587;
+                    smtp.Send(mail);
+                    ViewBag.Message = "Your Inquiry Has Been Sent! We will get in touch with you shortly.";
+                }
+            }
+            return View();
         }
 
     }
